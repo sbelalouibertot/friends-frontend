8fd4291ea68f8f103102c8bce6225a9b4e6e5166
Samy Belaloui-Bertot <samy@MacBook-Air-de-user.local>
2021-03-02 15:19:36 +0000

    init redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./redux/reducers/rootReducer";
+import thunk from 'redux-thunk'
+
+const middleware = [thunk]
+const enhancers = [];
+if (
+  process.env.NODE_ENV === "development" &&
+  typeof window != undefined &&
+  window.devToolsExtension
+) {
+  enhancers.push(
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  );
+}
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(...middleware), ...enhancers)
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
